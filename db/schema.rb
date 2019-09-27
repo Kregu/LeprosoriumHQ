19ced7132c59f6c436a8de56af423e5b22bac372
Sergey Gorokhov <sergey.login@gmail.com>
2019-09-28 01:27:40 +0300

    Create Comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_164328) do
+ActiveRecord::Schema.define(version: 2019_09_27_213843) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
